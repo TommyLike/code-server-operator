b3e73d6c7db64554ef445b9138b52558ee6a5bcd
TommyLike <hu.husheng@huawei.com>
2020-03-06 17:06:36 +0800

    Support recycle volumes when time exceedsd

---
+++ controllers/codeserver_recycle_cache.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2019 tommylikehu@gmail.com.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"k8s.io/apimachinery/pkg/types"
+	"sync"
+)
+
+type CodeServerActiveCache struct {
+	sync.RWMutex
+	Caches map[string]CodeServerActiveStatus
+}
+
+type CodeServerActiveStatus struct {
+	ProbeEndpoint  string
+	Duration       int64
+	FailureCount   int
+	NamespacedName types.NamespacedName
+}
+
+func (c *CodeServerActiveCache) AddOrUpdate(req CodeServerRequest) {
+	c.Lock()
+	defer c.Unlock()
+	if obj, found := c.Caches[req.resource.String()]; found {
+		obj.Duration = req.duration
+		obj.ProbeEndpoint = req.endpoint
+	} else {
+		c.Caches[req.resource.String()] = CodeServerActiveStatus{
+			ProbeEndpoint:  req.endpoint,
+			Duration:       req.duration,
+			FailureCount:   0,
+			NamespacedName: req.resource,
+		}
+	}
+}
+
+func (c *CodeServerActiveCache) Delete(req CodeServerRequest) {
+	c.Lock()
+	defer c.Unlock()
+	if _, found := c.Caches[req.resource.String()]; found {
+		delete(c.Caches, req.resource.String())
+	}
+}
+
+func (c *CodeServerActiveCache) DeleteFromName(req types.NamespacedName) {
+	c.Lock()
+	defer c.Unlock()
+	if _, found := c.Caches[req.String()]; found {
+		delete(c.Caches, req.String())
+	}
+}
+
+func (c *CodeServerActiveCache) BumpFailureCount(key string) {
+	c.Lock()
+	defer c.Unlock()
+	if obj, found := c.Caches[key]; found {
+		obj.FailureCount += 1
+	}
+}
+func (c *CodeServerActiveCache) Get(key string) *CodeServerActiveStatus {
+	c.RLock()
+	defer c.RUnlock()
+	if obj, found := c.Caches[key]; found {
+		return &obj
+	}
+	return nil
+}
+
+func (c *CodeServerActiveCache) GetKeys() []string {
+	var result []string
+	c.RLock()
+	defer c.RUnlock()
+	for k := range c.Caches {
+		result = append(result, k)
+	}
+	return result
+
+}
